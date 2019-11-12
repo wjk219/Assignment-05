fbd2c68d4082732bb3c3598ceae202193aaea3a9
Wes Klinvex <wjk219@lehigh.edu>
2019-11-12 10:45:29 -0500

    I edited the R file

---
+++ Assignment 05.R
@@ -0,0 +1,8 @@
+if (!require("data.table")) install.packages("data.table")
+library("data.table")
+header <- read.table("UNRATE.csv", header = TRUE,
+                     sep=",", nrow = 1)
+DF <- fread("UNRATE.csv", skip=1, sep=",",
+            header=FALSE, data.table=FALSE)
+setnames(DF, colnames(header))
+rm(header)
